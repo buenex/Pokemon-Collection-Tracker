a9effaec747720831537c1651090e78e0b52acf7
Gabriel Bueno <gabrielbs98@hotmail.com>
2026-02-21 22:16:06 -0300

    remove ssl

--- api/src/server.js
+++ api/src/server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL
-    ? { rejectUnauthorized: false }
-    : false,
+  connectionString: process.env.DATABASE_URL
 });
 
 async function initDB() {
