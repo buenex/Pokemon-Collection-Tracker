2938c31e40aeeaf678272f7c48e84fbbff88374a
Gabriel Bueno <gabrielbs98@hotmail.com>
2026-02-22 00:04:12 -0300

    pokemon_db

--- api/src/server.js
+++ api/src/server.js
@@ -82,6 +82,6 @@ app.post("/save", async (req, res) => {
 });
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log("Server running on port", PORT);
 });
